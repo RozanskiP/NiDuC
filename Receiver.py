29aaf2c0667048f69ba5ee6fc5941088bc065d7a
Pawel <pawir4@wp.pl>
2021-04-07 13:08:47 +0200

    Add structure

---
+++ Receiver.py
@@ -0,0 +1,16 @@
+
+
+
+class Receiver:
+
+    def __init__(self):
+        pass
+
+    def receiverFrameStopAndWait(self):
+        pass
+
+    def receiverFrameGoBackN(self):
+        pass
+
+    def receiverFrameSelectiveReapeat(self):
+        pass
---
+++ Sender.py
@@ -0,0 +1,26 @@
+
+
+
+class Sender:
+
+    def __init__(self):
+        pass
+
+    def sendFrameStopAndWait(self):
+        pass
+
+    def sendFrameGoBackN(self):
+        pass
+
+    def sendFrameSelectiveReapeat(self): 
+        pass
+
+    def addCodeParity(self):
+        pass
+    
+    def addCodeMirrosing(self):
+        pass
+
+    def addCodeCRC(self):
+        pass
+    
