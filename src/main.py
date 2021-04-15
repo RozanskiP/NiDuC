265bc2205215d949b583c5aa573305aeb975cf14
P.Sz <81773464+PiotrSzuba@users.noreply.github.com>
2021-04-15 23:30:20 +0200

    Add Photo handling

--- src/main.py
+++ src/main.py
@@ -6,7 +6,14 @@ from Coder import coderBit
 from Decoder import decodeBit
 from Generator import generateBit
 
-def main():
+from ImageFormating import ImgToBitArr
+from ImageFormating import BitArrToImg
+from ImageFormating import ShowResultImage
+from Plotter import ShowPlot
+
+DataList = []
+
+def main(Protocol_ID ,Code_ID, Probability ,Photo):
     receiver = Receiver()
     sender = Sender(receiver)
     receiver.setSender(sender)
@@ -31,9 +38,12 @@ def main():
     # print("Podaj prawdopodobienstwo przeklamac bitów w promilach: ")
     # propability = float(input(">>> "))
     SizeOfWindow = 9
-    chosenProtocol = 1
-    chosenCode = 3
-    propability = 0.90
+    chosenProtocol = Protocol_ID
+    chosenCode = Code_ID
+
+    ProbabilityList = [1.0, 0.99 , 0.95 , 0.90]
+    propability = ProbabilityList[Probability]
+    #propability = 0.90
 
     receiver.typeOfCode = chosenCode
     receiver.typeOfProtocol = chosenProtocol
@@ -44,7 +54,16 @@ def main():
     Frames = []
     SizeOfData = 8192 # zmienic na wczytywanie danych ze zdjecia
     SizeOfData = 1000 # do testowania mniejsza ilosc
-    generateBit(Frames, SizeOfData)
+
+    #mozna by wywalic if i elif jesli generacje bitów byłaby przed main()
+    #a sama lista bitów wchodziła do maina(BitList, Protocol_ID ,Code_ID, Probability ,Photo) jako argument
+    #wtedy tez wybór zdjecia byłby przed mainem a nie w mainie ale to chyba nie robi roznicy
+    #komentarze usunac po zrobienniu decyzji
+    if Photo == 0:
+        generateBit(Frames, SizeOfData)
+
+    elif Photo == 1:
+        Frames = ImgToBitArr(r"FLAGA.jpg")
 
     # zainicjalozowanie wartosci do glownego zbiornika na dane
     BER = 0
@@ -67,7 +86,11 @@ def main():
     if chosenProtocol == 3:
         sender.sendFrameSelectiveRepeat(masterlist)
 
+    if Photo == 0:
+        DataList.append(masterlist) 
 
+    elif Photo == 1:
+        BitArrToImg("Result.jpg", masterlist.data)
     # Szybkie testowanie 
     # Frames = []
     # SizeOfData = 4
@@ -85,4 +108,17 @@ def main():
     # decodeBit(listofbits, n)
 
 if __name__ == "__main__":
-    main()
+    main(1,3,3,0) #tak by wygladał main który zastałem przed edycją
+    for pr in range(1,4): #protokół
+        for c in range(1,4): #Kod
+            for p in range(2):#Prawdopodobienstwo
+               #main(pr,c,p,0)
+               pass
+    #main(1,3,3,1) #Mielenie zdjecia
+    #ShowResultImage() #Pokazanie zdjecia po "Mielonce"
+
+    ShowPlot(DataList)  #!!!! odpalac tylko jesli main jest W forach lub jest pojedynczy
+
+    #powstaje error przy gaszeniu okna z wykresami bo niby jak okno sie odpali z innego modułu niż tym gdzie jest __main__ (chyba)
+    #Fix przyjdzie szybko
+    
