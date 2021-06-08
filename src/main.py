3d9c1cb9ac748a93b880808ccd105cf7aada8fac
Pawel <pawir4@wp.pl>
2021-06-08 19:02:27 +0200

    Add code in testing

--- src/main.py
+++ src/main.py
@@ -11,7 +11,6 @@ from ImageFormating import ImgToBitArr
 from ImageFormating import BitArrToImg
 from ImageFormating import ShowResultImage
 
-
 def main():
     receiver = Receiver()
     sender = Sender(receiver)
@@ -86,4 +85,78 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
+
+# Main którym były testowane programy
+# def main(Protocol_ID ,Code_ID, Probability ,WindowSize ,Photo, Transmision_ID):
+#     receiver = Receiver()
+#     sender = Sender(receiver)
+#     transmision = Transmision()
+#     receiver.setSender(sender, transmision)
+
+#     #WinSizeList = [2, 4, 8 , 16, 32, 64, 128 , 256]
+#     WinSizeList = [32, 64, 128 , 256]
+#     SizeOfWindow = WinSizeList[WindowSize]
+#     chosenProtocol = Protocol_ID
+#     chosenCode = Code_ID
+#     ProbabilityList = [1.0, 0.99 , 0.98 , 0.97, 0.96, 0.95,0.94,0.93,0.92]
+#     propability = ProbabilityList[Probability]
+#     #propability = 0.90
+#     Channel_Name =["NULL","BSC","BEC","Hilb"]
+#     Protocol_Names = ["NULL", "Stop and wait", "Go back n","Selective Repeat"] 
+#     Code_Names = ["NULL", "Kod parzystości", "Kod dublowania", "CRC32", "sha256"]
+#     print(Channel_Name[trans]+" WinSize: "+str(WinSizeList[WindowSize])+" "+Protocol_Names[Protocol_ID]+" "+Code_Names[Code_ID]+" Prob: "+str(ProbabilityList[Probability]))
+
+#     receiver.typeOfCode = chosenCode
+#     receiver.typeOfProtocol = chosenProtocol
+#     receiver.typeOfTransmision = Transmision_ID
+#     sender.typeOfCode = chosenCode
+#     sender.typeOfProtocol = chosenProtocol
+
+#     # tutaj bedzie zdjęcie ładowane
+#     Frames = []
+#     SizeOfData = 8192 # zmienic na wczytywanie danych ze zdjecia
+#     SizeOfData = 256 # do testowania mniejsza ilosc
+
+#     #mozna by wywalic if i elif jesli generacje bitów byłaby przed main()
+#     #a sama lista bitów wchodziła do maina(BitList, Protocol_ID ,Code_ID, Probability ,Photo) jako argument
+#     #wtedy tez wybór zdjecia byłby przed mainem a nie w mainie ale to chyba nie robi roznicy
+#     #komentarze usunac po zrobienniu decyzji
+#     if Photo == 0:
+#         generateBit(Frames, SizeOfData)
+
+#     elif Photo == 1:
+#         Frames = ImgToBitArr(FileName)
+
+#     # zainicjalozowanie wartosci do glownego zbiornika na dane
+#     BER = 0
+#     E = 0
+#     ReceivedBits = 0
+#     time = 0
+
+#     masterlist = MasterList(Frames, BER, E, ReceivedBits, SizeOfWindow, chosenProtocol, chosenCode, propability, time, Transmision_ID)
+
+#     receiver.SizeOfWindow = SizeOfWindow
+#     sender.SizeOfWindow = SizeOfWindow
+#     if chosenProtocol == 1:
+#         sender.sendFrameStopAndWait(masterlist)
+
+#     if chosenProtocol == 2:
+#         sender.sendFrameGoBackN(masterlist)
+
+#     if chosenProtocol == 3:
+#         sender.sendFrameSelectiveRepeat(masterlist)
+
+#     if Photo == 0:
+#         DataList.append(masterlist) 
+
+#     elif Photo == 1:
+#         BitArrToImg("Result.jpg", masterlist.data)
+
+# if __name__ == "__main__":
+#     print("Main started")
+#     for trans in range(2,3):
+#         for WinSize in range(2): #Długosc ramki/okna
+#             for pr in range(1,4): #protokół
+#                 for c in range(1,5): #Kod
+#                     for p in range(2): #Prawdopodobienstwo
+#                         main(pr,c,p,WinSize,0,trans)
